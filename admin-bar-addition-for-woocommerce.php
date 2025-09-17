b77552adb80ee22f8c6483f473ecf2440223c206
karmacharya50 <samirkarmacharya68@gmail.com>
2025-09-17 23:50:43 +0545

    v1.4.2

--- admin-bar-addition-for-woocommerce.php
+++ admin-bar-addition-for-woocommerce.php
@@ -3,18 +3,18 @@
 Plugin Name: Admin Bar Addition for WooCommerce
 Plugin URI: https://wpfactory.com
 Description: Admin bar addition for WooCommerce.
-Version: 1.4.1
+Version: 1.4.2
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: admin-bar-addition-for-woocommerce
 Domain Path: /langs
-WC tested up to: 9.1
+WC tested up to: 10.1
 Requires Plugins: woocommerce
 */
 
 defined( 'ABSPATH' ) || exit;
 
-defined( 'ALG_WC_ADMIN_BAR_ADDITION_VERSION' ) || define( 'ALG_WC_ADMIN_BAR_ADDITION_VERSION', '1.4.1' );
+defined( 'ALG_WC_ADMIN_BAR_ADDITION_VERSION' ) || define( 'ALG_WC_ADMIN_BAR_ADDITION_VERSION', '1.4.2' );
 
 defined( 'ALG_WC_ADMIN_BAR_ADDITION_FILE' ) || define( 'ALG_WC_ADMIN_BAR_ADDITION_FILE', __FILE__ );
 
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Admin Bar Addition for WooCommerce ===
-Contributors: wpcodefactory, algoritmika, anbinder, karzin, omardabbas, kousikmukherjeeli
+Contributors: wpcodefactory, anbinder, karzin, omardabbas, kousikmukherjeeli
 Tags: woocommerce, admin bar addition, adminbar, admin bar
 Requires at least: 4.4
-Tested up to: 6.5
-Stable tag: 1.4.1
+Tested up to: 6.8
+Stable tag: 1.4.2
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -93,6 +93,10 @@ Admin bar addition for WooCommerce.
 1. Admin bar addition for WooCommerce.
 
 == Changelog ==
+
+= 1.4.2 - 17/09/2025 =
+* WC tested up to: 10.1.
+* Tested up to: 6.8.
 
 = 1.4.1 - 30/07/2024 =
 * WC tested up to: 9.1.
